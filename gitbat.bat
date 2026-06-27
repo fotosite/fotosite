7ca3bf04e8118e9aafc0f646770c05279c82724f
martinwagner-de <wagner@martinwagner.de>
2026-06-27 19:19:07 +0200

    iOS: global button user-select none in app.css

--- gitbat.bat
+++ gitbat.bat
@@ -1,4 +1,4 @@
 cd D:\mwa\Projekte\fotosite\Fotosite_V08\claudescode\fotosite
 git add -A
-git commit -m "iOS button feedback: mand dashboard Passkey-Banner - select-none, a-zu-button, active-Klassen"
+git commit -m "iOS: global button user-select none in app.css"
 git push
